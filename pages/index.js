f8c4df06cbfce4ad412e847dc8fa5b0cba830ec7
kitzian <72838760+kitzian@users.noreply.github.com>
2021-07-05 00:21:28 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -88,7 +88,7 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
             />
           )
         })}
-        <div className="flex flex-row justify-between mt-4 text-sm">
+        <div className="flex flex-row justify-center mt-4 text-sm">
           <div>
             Powered by{' '}
             <a href="https://workers.cloudflare.com/" target="_blank">
@@ -97,14 +97,6 @@ export default function Index({ config, kvMonitors, kvMonitorsLastUpdate }) {
             &{' '}
             <a href="https://flareact.com/" target="_blank">
               Flareact{' '}
-            </a>
-          </div>
-          <div>
-            <a
-              href="https://github.com/eidam/cf-workers-status-page"
-              target="_blank"
-            >
-              Get Your Status Page
             </a>
           </div>
         </div>
